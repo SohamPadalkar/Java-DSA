1f2bc1f2a26eccf3dbf89d3bcc7e303b4f2dbd2d
Soham Vilas Padalkar <bub56813@gmail.com>
2025-07-19 13:46:45 +0530

    FactorsOfNumber modified

--- src/Lecture2/FactorsOfNumber.java
+++ src/Lecture2/FactorsOfNumber.java
@@ -8,8 +8,14 @@ public class FactorsOfNumber {
         Scanner in = new Scanner(System.in);
 
         System.out.print("Enter the number: ");
-
         int num = in.nextInt();
+
+        System.out.println("Factors of the number " + num + " are:");
+        for (int i = 1; i <= num; i++) {
+            if (num % i == 0) {
+                System.out.print(" " + i);
+            }
+        }
 
 
     }
